d7635bcf5e76609d30f26bf6d3f8209bfd21a2b9
dev.kthines <dev.kthines@gmail.com>
2024-04-07 22:03:24 -0700

    basic api caller

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-﻿
+﻿function fetchPokemon() {
+    const pokemonName = document.getElementById('pokemonInput').value.toLowerCase();
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayPokemonInfo(data);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+            document.getElementById('pokemonInfo').innerText = 'Pokemon not found!';
+        });
+}
+
+function displayPokemonInfo(data) {
+    const pokemonInfo = document.getElementById('pokemonInfo');
+    pokemonInfo.innerHTML = `
+        <h2>${data.name.toUpperCase()}</h2>
+        <img src="${data.sprites.front_default}" alt="${data.name}">
+        <p>Height: ${data.height}</p>
+        <p>Weight: ${data.weight}</p>
+    `;
+}
